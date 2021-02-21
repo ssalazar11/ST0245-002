3f8cf44ed47ae6bb94fdaae7563027a552ed5d90
ssalazar11 <78051852+ssalazar11@users.noreply.github.com>
2021-02-21 14:49:07 -0500

    Update Taller4.java

--- talleres/taller04/Taller4.java
+++ talleres/taller04/Taller4.java
@@ -20,14 +20,14 @@ public class Taller4 {
 	* @return la suma
 	*/
 	public static int arrayMax(int[] array, int n) {
-		int max, temp, n;
+		int max, temp, i;
 		max=array[n];
 		if(n!=0){
 			temp=arrayMax(array, n-1);
 			if(temp>max){
-				return max;
+				max=temp
 			}
-			
+			return max;
 		}
 	}
     
