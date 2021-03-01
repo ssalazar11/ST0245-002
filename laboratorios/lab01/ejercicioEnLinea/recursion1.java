dc3a326b974e33d24abc05bf6bf798562391ef5b
csofia1408 <72955238+csofia1408@users.noreply.github.com>
2021-02-28 21:39:24 -0500

    Update recursion1.java

--- laboratorios/lab01/ejercicioEnLinea/recursion1.java
+++ laboratorios/lab01/ejercicioEnLinea/recursion1.java
@@ -1,4 +1,3 @@
-
 //Ejercicios CoginBat (Recursión 1)
 
 //Ejercicio 1(bunnyEars2)
@@ -13,10 +12,10 @@
 */
 
 public int bunnyEars2(int bunnies) {
-if( bunnies == 0){// O(n)
+if( bunnies == 0){// O(1)
     return 0;// O(1)
 } else{
-if(bunnies%2==0){ // O(n)
+if(bunnies%2==0){ // O(1)
    return 3 + bunnyEars2(bunnies-1);// O(1) + O(n-1)
 }else{
    return 2 + bunnyEars2(bunnies-1);// O(1)+ O(n-1)
@@ -24,7 +23,7 @@ if(bunnies%2==0){ // O(n)
   }
 }
 /*
-T(n)= O(n)+O(1)+O(n)+O(1)+O(n-1)+O(1)+ O(n-1)= O(2n)+O(3)+O(2n-1) = O(n)
+T(n)= O(1)+O(1)+O(1)+O(1)+O(n-1)+O(1)+ O(n-1)= O(1)+ O(n-1) = C + T(n-1) =  O (n)
 
 */
 
@@ -38,14 +37,14 @@ que corresponde al número de bloques, mediante recursividad.
  @return -El número total de bloques de una piramede que depende de el número de filas.
 */
 public int triangle(int rows) {
-if(rows == 0){//O(n)
+if(rows == 0){//C ,O(1)
     return 0;//O(1)
 }else{
 return rows+triangle(rows-1);// O(1) + O(n-1)
  } 
 }
 
-// T(n) = n + 1 + 1 + n - 1 = 1 + 2n = O(1 + 2n) = O(n)
+// T(n) = n + 1 + 1 + n - 1 = 1 + 2n = O(1 + 2n) =C + T(n-1), O(n)
 
 //Ejercicio 3(sumDigits)
 
@@ -63,7 +62,7 @@ if(n==0){ // O(1)
 return n%10+sumDigits(n/10); // O(1) + O(n/10-1)
  }
 }  
-//  T(n) = n/10 + 1 + 1 + n/10 - 1 = 1 + 2n = O(log n)
+//  T(n) = n/10 + 1 + 1 + n/10 - 1 = 1 + 2n = C+ t(n/10) ,O(log n)
 
 //Ejercicio 4(count7)
 
@@ -85,7 +84,7 @@ public int count7(int n) {
         }
    }   
 }
-// T(n) = 1 + 1 + 1 + 1 + n + n/10 + n/10  = 1 + 4n = O(1 + 4n) = O(log n)
+// T(n) = 1 + 1 + 1 + 1 + n + n/10 + n/10  = 1 + 4n = O(1 + 4n) = C+ t(n/10), O(log n)
 //Ejercicio 5(count8)
 
 /*Calcular las veces que se repite el 8 en un número no negativo, teniendo en cuenta si se repite 
@@ -113,4 +112,4 @@ public int count8(int n) {
   }   
 }
 
-//O(1)+O(1)+O(1)+O(1)+O(1)+ O(n/10)+O(1) + O(n/10) +O(n/10) = O(log n)
+//O(1)+O(1)+O(1)+O(1)+O(1)+ O(n/10)+O(1) + O(n/10) +O(n/10) = C+ t(n/10), O(log n)
