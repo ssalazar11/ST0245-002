c2d93d7715038d4746e049e81ed24f3882d62955
ssalazar11 <78051852+ssalazar11@users.noreply.github.com>
2021-03-28 16:36:41 -0500

    Update LeerDatos.py

--- laboratorios/lab03/codigo/LeerDatos.py
+++ laboratorios/lab03/codigo/LeerDatos.py
@@ -1,3 +1,8 @@
+#Laboratorio 3 Estructuras de datos y algortimos
+
+#@authors Samuel Salazar salazar/Carla Sofía Rendon
+#@version 1
+
 import pandas as pd
 
 dataVertices = pd.read_csv('Vertices.csv')
@@ -9,12 +14,12 @@ dataArcos.columns = ["ID","ID1","Distancia","Nombre"]
 print(dataArcos.head())
 
 diccArcos={}
-for i in :
-    diccArcos[(dataArcos.iloc[i]['ID'], dataArcos.iloc[i]['ID1'])]=
+for i in dataArcos:
+    diccArcos[(dataArcos.iloc[i]['ID'], dataArcos.iloc[i]['ID1'])]=diccArcos
 print("El diccionario de los arcos es: "+diccArcos)
 
 diccVert={}
-for i in :
-    diccVert[(dataVertices.iloc[i]['ID'])]=
+for i in dataVertices:
+    diccVert[(dataVertices.iloc[i]['ID'])]=diccVert
 print("El diccionario de los vertices es: "+diccVert)
 
