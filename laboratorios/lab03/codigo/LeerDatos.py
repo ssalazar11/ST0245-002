2d8424f394e04147713ace5a17e9ad87608d9155
ssalazar11 <78051852+ssalazar11@users.noreply.github.com>
2021-03-28 13:59:44 -0500

    Update LeerDatos.py

--- laboratorios/lab03/codigo/LeerDatos.py
+++ laboratorios/lab03/codigo/LeerDatos.py
@@ -8,5 +8,13 @@ dataArcos = pd.read_csv('Arcos.csv')
 dataArcos.columns = ["ID","ID1","Distancia","Nombre"]
 print(dataArcos.head())
 
-...
-    
+diccArcos={}
+for i in :
+    diccArcos[(dataArcos.iloc[i]['ID'], dataArcos.iloc[i]['ID1'])]=
+print("El diccionario de los arcos es: "+diccArcos)
+
+diccVert={}
+for i in :
+    diccVert[(dataVertices.iloc[i]['ID'])]=
+print("El diccionario de los vertices es: "+diccVert)
+
