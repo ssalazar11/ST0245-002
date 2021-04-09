3268e0774bfd44e081a05d3c72033662dedf4175
ssalazar11 <78051852+ssalazar11@users.noreply.github.com>
2021-04-09 08:48:25 -0500

    Update taller9.py

--- talleres/taller09/taller9.py
+++ talleres/taller09/taller9.py
@@ -1,12 +1,17 @@
 # Punto 1
+import numpy as np
+
 class HashTable():
     
     def __init__(self):
-          # hash_function
+        tabla=np.zeros(10)
+        
     def funcion_hash(self, k):
+        return (int(k[0]))%10
     
     def get(self, k):
-       
+        return tabla[funcion_hash(k)]
 
-    def put(self, k, v):       
+    def put(self, k, v):
+        tabla[funcion_hash(k)]=v
         
