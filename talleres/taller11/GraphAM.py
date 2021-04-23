9007b1549617d602288ad36b4095f76bf87d1b9d
ssalazar11 <78051852+ssalazar11@users.noreply.github.com>
2021-04-23 09:51:39 -0500

    Update GraphAM.py

--- talleres/taller11/GraphAM.py
+++ talleres/taller11/GraphAM.py
@@ -1,17 +1,29 @@
+import numpy as np
+
 class GraphAm:
 
     def __init__(self, size):
-
+        self.size=size
+        self.matriz=np.zeros(size,size)
 
     def getEdges(self):
 
 
     def getWeight(self, source, destination):
+        return self.matriz[source][destination]
 
 
     def addArc(self, source, destination, weight):
+        self.matriz[source][destination]=weight
 
     def getSuccessors(self, vertex):
+        sucesor=[]
+        for n, y in enumerate(self.matriz[vertex]):
+            if y!=0:
+                sucesor.append(n)
+        return sucesor
+        
 
     def __str__(self):
+        return self.matriz
 
